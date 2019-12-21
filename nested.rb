2f67e961fd6105ed48c6fecd154411c2a6f0b825
Adam  Shappy <ajs11300@gmail.com>
2019-12-21 20:39:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      #alans_new_info = "GUI"
 programmer_hash[:alan_kay][:known_for] = "GUI"
-#programmer_hash[:alan_kay][:known_for][:alans_new_info] = "GUI"
 
 programmer_hash
      
@@ -131,5 +130,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+programmer_hash
 
 end
