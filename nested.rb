e7874b1e46f18a67e9989984e32e3c2289c1b5a7
Adam  Shappy <ajs11300@gmail.com>
2019-12-21 20:03:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,8 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto]= {}
-programmer_hash{:yukihiro_matsumoto][:known_for]
-programmer_hash{:yukihiro_matsumoto][:languages]
+programmer_hash{:yukihiro_matsumoto][:known_for] ={}
+programmer_hash{:yukihiro_matsumoto][:languages] = {}
 end
 
 def changing_alan
