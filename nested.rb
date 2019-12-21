228ff860c46d37f98d043bf7d0336de7bc6789d0
Adam  Shappy <ajs11300@gmail.com>
2019-12-21 20:23:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,6 +86,7 @@ def adding_matz
   
   programmer_hash[:yukihiro_matsumoto] = {}
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["C"]
   
 end
 
